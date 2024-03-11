b0e13eadb64e3a67c0b394496fc410e695e02abc
TusharSatija <tusharsatija737@gmail.com>
2024-03-11 17:21:03 +0530

    updates Snake game

--- Lecture-24/snake/app.js
+++ Lecture-24/snake/app.js
@@ -15,7 +15,7 @@ let Snake={
     {
         for(let i=0;i<this.init;i++)
         {
-            this.cell.push({
+            this.cells.push({
                 x:i,
                 y:0
             })
@@ -25,13 +25,46 @@ let Snake={
     {
         for( let cell of this.cells)
         {
-            ctx.fillRect(cell.x*cs,cell.y*cs,cs,cs);
+            ctx.fillRect(cell.x*cs,cell.y*cs,cs-1,cs-1);
         }    
-    }
+    },
 
     UpdateSnake:function()
     {
         
+        const Headx=this.cells[this.cells.length-1].x;
+        const heady=this.cells[this.cells.length-1].y;
+
+
+        let nextX;
+        let nexty;
+
+        if(this.direction==='down')
+        {
+            nextX=Headx;
+            nexty=heady+1;
+        }
+        else if(this.direction==='up')
+        {
+            nextX=Headx;
+            nexty=heady-1;
+        }
+        else if(this.direction==='left')
+        {
+            nextX=Headx-1;
+            nexty=heady;
+        }
+        else{
+            nextX=Headx+1;
+            nexty=heady;
+        }
+
+        this.cells.push({
+            x:nextX,
+            y:nexty
+        });
+
+        this.cells.shift();
     }
     
 
@@ -40,49 +73,47 @@ let Snake={
 function inital()
 {
     Snake.CreateSnake();
+    function keyPressed(e)
+    {
+        if(e.key==='ArrowDown')
+        {
+            Snake.direction='down';
+        }
+        else if(e.key==='ArrowUp')
+        {
+            Snake.direction='up';
+        }
+        else if(e.key==='ArrowLeft')
+        {
+            Snake.direction='left';
+        }
+        else {
+            Snake.direction='right';
+        }
+    }
+    document.addEventListener('keydown',keyPressed);
 }
 
 function update()
 {
-    
+    Snake.UpdateSnake();
 }
 
 function draw()
 {
+    ctx.clearRect(0,0,W,H);
+    ctx.fillStyle='yellow';
     Snake.DrawSnake();
 }
 
+inital();
 function GameLoop()
 {
+    update();
+    draw();
+} 
 
-}
-// function init()
-// {
-//     ctx.fillRect(W,H,100,100);
-// }
-// function update()
-// {
-//     W+=100;
-//     // console.log("inside update");
-     
-// }
-// function draw()
-// {
-//     ctx.clearRect(0,0,1200,750);
-//     ctx.fillRect(W,H,100,100);
-//     // console.log("inside draw");
-
-// }
-
-// init();
-// function GameLoop()
-// {
-//     update();
-//     draw();
-// }
-// setInterval(GameLoop,100);
-
-
+setInterval(GameLoop,100);
 
 
 
